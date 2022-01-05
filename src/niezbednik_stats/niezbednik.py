6ff72efa0bbc6be51753d154e150dac7f770da7f
kajetan <katek1094@interia.pl>
2022-01-05 22:49:32 +0100

    added columns for dates dictionary

--- src/niezbednik_stats/niezbednik.py
+++ src/niezbednik_stats/niezbednik.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import datetime
+from collections import namedtuple
 
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
@@ -46,37 +47,51 @@ def get_account_raw_data(s: requests.Session, account_name):
     return r.json()
 
 
+Record = namedtuple('Record', 'date value')
+OfferData = namedtuple('OfferData', 'id_number records')
+
+
 def format_account_data(data):
     formatted_data = []
-    for dt in data['series']:
-        arr = []
-        for record in dt['data']:
-            arr.append(record[1])
-        formatted_data.append((dt['name'], arr))
+    for offer in data['series']:
+        records = []
+        for record in offer['data']:
+            timestamp_in_ms, position = record
+            record_date = datetime.datetime.fromtimestamp(timestamp_in_ms / 1000).date()
+            records.append(Record(record_date, position))
+        formatted_data.append(OfferData(offer['name'], tuple(records)))
+
     return formatted_data
 
 
-def create_excel(data):
-    wb = Workbook()
-    ws = wb.active
-    row = 1
-    col = 2
-
+def generate_columns_for_dates_dict():
+    result = {}
     start_date = datetime.date.today()
     day_count = 30
-    for date in (start_date - datetime.timedelta(day_count - n) for n in range(day_count)):
-        ws.cell(row=row, column=col).value = date.strftime('%d-%m')
-        col += 1
-    row += 1
-    col = 1
-    for id_number, records in data:
-        ws.cell(row=row, column=col).value = id_number
-        col += 1
-        for record in records:
-            ws.cell(row=row, column=col).value = record
-            col += 1
+    for index, date in enumerate(start_date - datetime.timedelta(day_count - n) for n in range(day_count)):
+        result[date] = index + 2
+    return result
+
+
+columns_for_dates = generate_columns_for_dates_dict()
+
+
+def create_excel(data: [OfferData]):
+    wb = Workbook()
+    ws = wb.active
+
+    # WRITE DATES
+    for date, column in columns_for_dates.items():
+        ws.cell(row=1, column=column).value = date.strftime('%d-%m')
+
+    # WRITE DATA
+    row = 2
+    for offer_data in data:
+        ws.cell(row=row, column=1).value = offer_data.id_number
+        for record in offer_data.records:
+            ws.cell(row=row, column=columns_for_dates[record.date]).value = record.value
         row += 1
-        col = 1
+
     wb.save('/home/kajetan/Desktop/handlowiec-rs-pl.xlsx')
 
 
@@ -85,3 +100,5 @@ login(s)
 raw_data = get_account_raw_data(s, 'handlowiec-rs-pl')
 results = format_account_data(raw_data)
 create_excel(results)
+
+# print(datetime.datetime.fromtimestamp(1639004400).strftime("%A, %B %d, %Y %I:%M:%S"))
