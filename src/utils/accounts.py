d4f4cff67bf94fb8b2d49ce6b24695cac9692c1f
kajetan <katek1094@interia.pl>
2022-03-12 11:05:35 +0100

    updated AdsAccount Class

--- src/utils/accounts.py
+++ src/utils/accounts.py
@@ -2,15 +2,36 @@ import openpyxl
 
 
 class AdsAccount:
-    def __init__(self, name: str, monthly_budget: int, revenue_target: int, priority: int, is_graphic: bool, ):
-        self.name = name
+    VALID_PRIORITIES = (1, 2, 3)
+
+    def __init__(self, name: str, priority: int, monthly_budget: int = None, revenue_target: int = None,
+                 is_graphic: bool = None):
+        self.name = name.strip()
         self.monthly_budget = monthly_budget
         self.revenue_target = revenue_target
-        self.priority = priority
+        self.priority = self._validate_priority(priority)
         self.is_graphic = is_graphic
 
     def __repr__(self):
         return self.name
+
+    @property
+    def weekly_budget(self) -> float:
+        if self.monthly_budget:
+            return round(self.monthly_budget / 4.345)
+
+    @property
+    def daily_budget(self) -> float:
+        if self.monthly_budget:
+            return round(self.daily_budget / 30.437)
+
+    def _validate_priority(self, priority: int) -> int:
+        if priority in self.VALID_PRIORITIES:
+            return priority
+        else:
+            print(priority)
+            print(self.name)
+            raise ValueError(f'Priority has to be one of these values: {self.VALID_PRIORITIES}')
 
 
 def get_all_accounts_from_excel(*, priority: int = False):
@@ -22,7 +43,6 @@ def get_all_accounts_from_excel(*, priority: int = False):
 
     row = 3
     col = 1
-
     while True:
         account_name = ws.cell(row=row, column=col).value
         account_monthly_budget = ws.cell(row=row, column=col + 1).value
@@ -31,7 +51,7 @@ def get_all_accounts_from_excel(*, priority: int = False):
         account_is_graphic = ws.cell(row=row, column=col + 5).value == 'tak'
         if account_name:
             accounts_list.append(
-                AdsAccount(account_name, account_monthly_budget, account_revenue_target, account_priority,
+                AdsAccount(account_name, account_priority, account_monthly_budget, account_revenue_target,
                            account_is_graphic))
             row += 1
         else:
